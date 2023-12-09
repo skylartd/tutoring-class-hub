cea427b78fbdc4a1ff3392d57e588b00fc777c15
skylartd <thanhduong0880@gmail.com>
2023-12-09 19:30:23 +0700

    Init

---
+++ src/main/java/com/tutoring/classhub/ClasshubApplication.java
@@ -0,0 +1,13 @@
+package com.tutoring.classhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClasshubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClasshubApplication.class, args);
+	}
+
+}
